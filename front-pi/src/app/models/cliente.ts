2c8084d764a649c663e1d8c247575e9ac6e84cb6
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-02 17:51:27 -0300

    refactor: client atributtes

--- front-pi/src/app/models/cliente.ts
+++ front-pi/src/app/models/cliente.ts
@@ -1,12 +1,12 @@
 export class Cliente {
-  id: number;
-  nome: string;
-  email: string;
-  cpf: string;
-  senha: string;
+  id?: number;
+  nome?: string;
+  email?: string;
+  cpf?: string;
+  senha?: string;
 
 
-  constructor (id: number, nome: string, email: string, cpf: string, senha: string) {
+  constructor (id?: number, nome?: string, email?: string, cpf?: string, senha?: string) {
     this.id = id;
     this.nome = nome;
     this.email = email;
