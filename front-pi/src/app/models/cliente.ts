0183aa8c6f79b7cf719073ca2da0e9a7d0d133ee
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-02 14:17:14 -0300

    feat: add client models

---
+++ front-pi/src/app/models/cliente.ts
@@ -0,0 +1,15 @@
+export class Cliente {
+  id: number;
+  nome: string;
+  email: string;
+  senha: string;
+
+
+  constructor (id: number, nome: string, email: string, senha: string) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+  }
+
+}
