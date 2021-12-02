55a2656c5370b5b2cddb23e8fe5c8d9497ae2f2e
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-02 11:59:41 -0300

    chore: add http module

--- front-pi/src/app/app.module.ts
+++ front-pi/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './views/cliente/cliente.component';
@@ -12,7 +12,8 @@ import { ClienteComponent } from './views/cliente/cliente.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
