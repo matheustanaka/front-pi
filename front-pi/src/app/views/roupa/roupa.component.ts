320e4eda328cc4726ed0b29017d4fc7f4a344cae
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-03 14:54:41 -0300

    feat: add functionalities inside of roupa component

--- front-pi/src/app/views/roupa/roupa.component.ts
+++ front-pi/src/app/views/roupa/roupa.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Roupa } from 'src/app/models/roupa';
+import { RoupaService } from 'src/app/services/roupa.service';
 @Component({
   selector: 'app-roupa',
   templateUrl: './roupa.component.html',
   styleUrls: ['./roupa.component.css']
 })
 export class RoupaComponent implements OnInit {
+  roupas = new Array<Roupa>();
 
-  constructor() { }
+  roupaEdicao?: Roupa = undefined;
+
+  estaEditando = false;
+
+  constructor(private roupaService: RoupaService) { }
 
   ngOnInit(): void {
+    this.listarRoupas();
   }
 
+  listarRoupas(): void {
+    this.roupaService.listar().subscribe(roupas => {
+      this.roupas = roupas;
+    })
+  }
+
+  salvar(): void {
+    if(this.roupaEdicao == undefined) {
+      return
+    }
+    if(!this.estaEditando){
+      this.roupaService.inserir(this.roupaEdicao).subscribe(() => {
+        this.listarRoupas();
+        this.cancelar();
+      })
+    } else {
+      this.roupaService.atualizar(this.roupaEdicao).subscribe(()=> {
+        this.listarRoupas();
+        this.cancelar();
+      })
+    }
+  }
+
+  novoCliente() {
+    this.roupaEdicao = new Roupa();
+    this.estaEditando = false;
+  }
+
+  cancelar() {
+    this.roupaEdicao = undefined;
+    this.estaEditando = false;
+  }
+
+  selecionarCliente(roupa: Roupa) {
+    this.roupaEdicao = roupa;
+    this.estaEditando = true;
+  }
+
+  excluir(roupa: Roupa) {
+    const resposta = confirm(`A ${roupa.tipoRoupa} da ${roupa.marca} foi excluída`);
+    if(resposta && roupa.id && roupa) {
+      this.roupaService.excluir(roupa.id).subscribe(() => {
+        this.listarRoupas();
+      })
+    }
+  }
 }
