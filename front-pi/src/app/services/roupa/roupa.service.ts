44c30a802f6440340570b606e9b2cb1be9156832
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-03 16:24:49 -0300

    replace: add roupa folder in services

--- front-pi/src/app/services/roupa/roupa.service.ts
+++ front-pi/src/app/services/roupa/roupa.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Roupa } from '../models/roupa';
+import { Roupa } from '../../models/roupa';
 
 const roupaLink = 'http://localhost:8080/exemplo-cadastro/webapi/roupas';
 
