97b5da9fda597db176ef67f70683fb4557d9c067
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-04 23:07:00 -0300

    refactor: rename numero venda

--- front-pi/src/app/models/venda.ts
+++ front-pi/src/app/models/venda.ts
@@ -1,18 +1,20 @@
 import {Cliente} from './cliente';
 import {Pagamento} from './pagamento';
 import { ItemVenda } from './itemVenda';
+import { Roupa } from './roupa';
 
 export class Venda {
   id_venda?: number;
-  numero_venda?: number;
+  numeroVenda?: number;
   dataVenda?: Date;
   itensVendidos = new Array <ItemVenda>();
   cliente = new Cliente();
   pagamento = new Pagamento();
+  itemVenda = new ItemVenda();
 
-  constructor(id_venda?: number, numero_venda?: number, dataVenda?: Date) {
+  constructor(id_venda?: number, numeroVenda?: number, dataVenda?: Date) {
     this.id_venda = id_venda;
-    this.numero_venda = numero_venda;
+    this.numeroVenda = numeroVenda;
     this.dataVenda = dataVenda;
   }
 }
