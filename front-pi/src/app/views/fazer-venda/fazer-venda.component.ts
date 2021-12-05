4e3e2ef966c59a5de1ea9d6461256158fa03bb13
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-04 23:08:48 -0300

    feat: add fazer venda component

--- front-pi/src/app/views/fazer-venda/fazer-venda.component.ts
+++ front-pi/src/app/views/fazer-venda/fazer-venda.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Roupa } from 'src/app/models/roupa';
+import { ItemVenda } from 'src/app/models/itemVenda';
+import { Cliente } from 'src/app/models/cliente';
+import { Pagamento } from 'src/app/models/pagamento';
+import { Venda } from 'src/app/models/venda';
+import { VendaService } from 'src/app/services/venda/venda.service';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
+import { RoupaService } from 'src/app/services/roupa/roupa.service';
+import { PagamentoService } from 'src/app/services/pagamento/pagamento.service';
+
 
 @Component({
   selector: 'app-fazer-venda',
@@ -7,9 +17,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FazerVendaComponent implements OnInit {
 
-  constructor() { }
+  vendas = new Array<Venda>();
+  venda = new Venda();
+
+  vendaEdicao?: Venda = undefined;
+  estaEditando = false;
+
+  clientes = new Array<Cliente>();
+  pagamentos = new Array<Pagamento>();
+  roupas = new Array<Roupa>();
+
+  roupa = new Roupa();
+  quantidadeVendida = 1;
+
+
+  constructor(private vendaService: VendaService, private clienteService: ClienteService, private roupaService: RoupaService, private pagamentoService: PagamentoService) { }
 
   ngOnInit(): void {
+    this.listarFazerVenda();
+  }
+
+  listarFazerVenda(): void {
+    this.clienteService.listar().subscribe(clientes => {
+      this.clientes = clientes;
+    })
+
+    this.pagamentoService.listar().subscribe(pagamentos => {
+      this.pagamentos = pagamentos;
+    })
+
+    this.roupaService.listar().subscribe(roupas => {
+      this.roupas = roupas;
+    })
+
+    this.vendaService.relatorio().subscribe(vendas => {
+      this.vendas = vendas;
+    })
+  }
+
+  novaVenda() {
+    this.vendaEdicao = new Venda();
+    this.estaEditando = false;
+  }
+
+  selecionarVenda(venda: Venda) {
+    this.vendaEdicao = venda;
+    this.estaEditando = true;
+  }
+
+  adicionarRoupa () {
+    let itemVenda = new ItemVenda();
+    itemVenda.roupa.id = this.roupa.id;
+    itemVenda.roupa.tipoRoupa = this.roupa.tipoRoupa;
+    itemVenda.quantidadeVendida = this.quantidadeVendida;
+    itemVenda.precoVendido = this.roupa.preco;
+
+    this.venda.itensVendidos.push(itemVenda);
+  }
+
+  cancelar() {
+    this.vendaEdicao = undefined;
+    this.estaEditando = false;
+  }
+
+  fazerVenda() {
+    this.vendaService.fazerVenda(this.venda).subscribe(() => {
+      alert("Venda Efetuada");
+    })
   }
 
 }
