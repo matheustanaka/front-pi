44fdc2f547173a62807b18297bf003529950d9f0
matheus.lima <matheus.lima@nava.com.br>
2021-12-06 15:21:56 -0300

    Criação do front com angular Material.

--- front-pi/src/app/app.module.ts
+++ front-pi/src/app/app.module.ts
@@ -9,6 +9,17 @@ import { RoupaComponent } from './views/roupa/roupa.component';
 import { PagamentoComponent } from './views/pagamento/pagamento.component';
 import { VendaComponent } from './views/relatorioVenda/venda.component';
 import { FazerVendaComponent } from './views/fazer-venda/fazer-venda.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
@@ -23,9 +34,20 @@ import { FazerVendaComponent } from './views/fazer-venda/fazer-venda.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
